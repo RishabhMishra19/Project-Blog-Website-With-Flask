803986fbcf2b8242cb712f9f694cf9d6e423de2d
Rishabh Mishra <rishabh@osfin.ai>
2024-04-03 12:36:38 +0530

    done

--- blog/constants.py
+++ blog/constants.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'mynew.db') #os.environ.get('MY_SQLALCHEMY_DATABASE_URI')
 APP_SECRET_KEY = 'rishabh' #os.environ.get('APP_SECRET_KEY')
-EMAIL_USER = os.environ.get('EMAIL_USER')
-EMAIL_PASS = os.environ.get('EMAIL_PASS')
+EMAIL_USER = 'pndtrishabh91@gmail.com' #os.environ.get('EMAIL_USER')
+EMAIL_PASS = 'xxdl gmax kpcq zanb' #os.environ.get('EMAIL_PASS')
